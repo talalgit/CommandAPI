0667fe8c5e66d0363c28beb8c2cfbd728f413f52
talalgit <47065888+talalgit@users.noreply.github.com>
2020-10-18 15:31:47 +0300

    Initial commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CommandAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommandsController : ControllerBase
+    {
+
+        [HttpGet]
+        public  ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[]
+            {
+                "This", "is", "hard", "code"
+            };
+        }
+
+    }
+}
